afb45e6ac2f7552362891b158ab71ff1bd8dd476
Anibal <aniale16@gmail.com>
2024-11-13 16:14:45 -0400

    MPV

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-const crearCanción = (informacioncancion) => {}
-
-
+const crearCanción = (informacioncancion) => {
     const div = document.createElement('div')
     div.setAttribute('class', 'songlist__song')
     div.innerHTML = `
-        <img src="/assets1/covermini.png" alt="" class="songlist__song__cover">
+        <img src="${"https://api.institutoalfa.org/api/songs/image/" + informacioncancion.image.filename}" alt="" class="songlist__song__cover">
         <div class="songlist__song__text">
-            <h1 class="songlist__song__text__title">DIE FOR YOU</h1>
-            <p class="songlist__song__text__artist">Starset</p>
+            <h1 class="songlist__song__text__title">${informacioncancion.title}</h1>
+            <p class="songlist__song__text__artist">${informacioncancion.author}</p>
         </div>
     `
+    
+    div.addEventListener('click', () => {
+        document.getElementById('song-front').setAttribute('src', "https://api.institutoalfa.org/api/songs/image/" + informacioncancion.image.filename)
+
+        document.getElementById('song-audio').setAttribute('src', "https://api.institutoalfa.org/api/songs/audio/" + informacioncancion.audio.filename)
+
+        document.getElementById('song-title').innerHTML = informacioncancion.title
+
+        document.getElementById('song-author').innerHTML = informacioncancion.author
+
+
+    })
+
+    
     return div
+}
+
+document.getElementById('play').addEventListener('click', () => {
+    const audio = document.getElementById('song-audio')
+    if (audio.pause) {
+        audio.onplay()} else {audio.pause}
+})
+
+axios.get('https://api.institutoalfa.org/api/songs').then((res) => {
+    const songs = res.data.songs
+
+    songs.map((song) => {
+        document.getElementById('container').appendChild(crearCanción(song))
+    })
+})
